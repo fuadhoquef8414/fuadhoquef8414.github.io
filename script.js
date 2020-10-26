0edb0f9a09f26405eab14e34e78d54d56f9de586
fuadhoquef8414 <73479692+fuadhoquef8414@users.noreply.github.com>
2020-10-26 09:40:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Fuad Farhan"
+let userName = "fuadhoquef8414";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
